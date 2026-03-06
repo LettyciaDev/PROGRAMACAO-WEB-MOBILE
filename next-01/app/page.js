fcaa36b4d20106568bd4ce8f9e86e3da73dbbc55
Lettycia Vitória <163659294+LettyciaDev@users.noreply.github.com>
2026-03-06 15:00:56 +0000

    mini-bio

--- next-01/app/page.js
+++ next-01/app/page.js
@@ -2,22 +2,36 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-500 justify-center bg-cyan-700 font-sans">
-      <main className="flex w-full max-w-3xl flex-col items-center space-y-20 py-20 px-16  sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Olá, turma de programação
-          </h1>
-        </div>
-      </main>
+    <div className="w-full min-h-screen bg-[#fec7fe] flex flex-col justify-center items-center">
+      
+      <div className="w-[80%] h-[75vh] bg-[#d797e7] rounded-xl shadow-lg p-12 flex flex-col gap-20">
+
+           <div className="flex justify-center items-center gap-2">
+              <Image className="rounded-full object-cover border-2 border-white"
+                src="/photo.jpg"
+                alt="profile photo"
+                width={100}
+                height={100}/>
+              <div className="mr-auto">
+                 <h1 className="text-1xl font-bold text-gray-800 leading-tight">Lettycia Vitória</h1>
+                 <p className="text-sm text-purple-900 font-medium">Dev Back-end</p>
+              </div>
+               <Image className="" 
+                src="/plant.png"
+                alt="decoração de planta"
+                width={60}
+                height={60}/>
+          </div>
+
+          <div className="flex justify-center text-gray-800 leading-relaxed">
+            <p className="w-3/4">Olá, me chamo Lettycia Vitória, tenho 19 anos, sou entusiasta de tecnologia e estudante de ciência da computação. 
+              Sou cristã praticante. Amo cultivar plantas, práticar esportes, assistir filmes e séries. Estou sempre disponível 
+              a aprender algo novo, e estou iniciando minha carreira profissional como desenvolvedora back-end</p>
+          </div>  
+
+      </div>
     </div>
   );
 }
+
+   
